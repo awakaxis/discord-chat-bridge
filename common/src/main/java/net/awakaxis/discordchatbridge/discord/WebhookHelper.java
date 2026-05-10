056a70c1da65ffb8d5ed2043e2f4390a12c719f0
awakaxis <awakaxis@gmail.com>
2026-05-09 22:43:14 -0700

    feat: make server avatar url and player headshot helper publicly accessible

--- common/src/main/java/net/awakaxis/discordchatbridge/discord/WebhookHelper.java
+++ common/src/main/java/net/awakaxis/discordchatbridge/discord/WebhookHelper.java
@@ -5,6 +5,13 @@ import club.minnced.discord.webhook.send.WebhookMessage;
 import club.minnced.discord.webhook.send.WebhookMessageBuilder;
 
 public final class WebhookHelper {
+
+    public static final String SERVER_AVATAR = "https://cdn.discordapp.com/attachments/1127684842176380938/1496999492296380499/server-icon.png?ex=69ebeceb&is=69ea9b6b&hm=ba4bd7f9eb2bb1b324ef68799de2148c8b76724765809dd45beace01d59bad1a&";
+
+    public static String getPlayerHeadshot(String username) {
+        return "https://minotar.net/avatar/" + username;
+    }
+
     public static void sendPlayerMessage(String username, String content, String url) {
         try (WebhookClient client = WebhookClient.withUrl(url)) {
             client.send(buildForPlayer(username, content));
@@ -18,10 +25,10 @@ public final class WebhookHelper {
     }
 
     private static WebhookMessage buildForPlayer(String username, String content) {
-        return new WebhookMessageBuilder().setUsername(username).setAvatarUrl("https://minotar.net/avatar/" + username).setContent(content).build();
+        return new WebhookMessageBuilder().setUsername(username).setAvatarUrl(getPlayerHeadshot(username)).setContent(content).build();
     }
 
     private static WebhookMessage buildForServer(String content) {
-        return new WebhookMessageBuilder().setUsername("Server").setAvatarUrl("https://cdn.discordapp.com/attachments/1127684842176380938/1496999492296380499/server-icon.png?ex=69ebeceb&is=69ea9b6b&hm=ba4bd7f9eb2bb1b324ef68799de2148c8b76724765809dd45beace01d59bad1a&").setContent(content).build();
+        return new WebhookMessageBuilder().setUsername("Server").setAvatarUrl(SERVER_AVATAR).setContent(content).build();
     }
 }
